31211edbf875e89548dca50d1d6b855c53ec8907
swami701 <swami701search@gmail.com>
2016-07-02 12:28:23 +0800

    Add with dates

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('/', function (req, res) {
   res.send('hello world');
 });
 
-app.get('/importparse', function (req, res) {
+app.post('/importparse', function (req, res) {
   console.log("Starting to import data to parse....");
   var json = require('./json/' + process.env.PARSE_CLASS + '.json');
   var existingJson = require('./json/' + process.env.PARSE_EX_NAME + '.json');
@@ -23,6 +23,37 @@ app.get('/importparse', function (req, res) {
   for (var i = 0; i < parseObjs.length; i++) {
     var obj = parseObjs[i];
     if (existingObjs.hasOwnProperty(obj["objectId"])) {
+      var ParseObj = Parse.Object.extend(process.env.PARSE_CLASS);
+      var parseObj = new ParseObj();
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          if (key !== 'objectId') {
+            parseObj.set(key, obj[key]);
+          } else {
+            parseObj.set("oldEventId", obj[key]);
+          }
+        }
+      }
+      objsToSave.push(parseObj);
+    }
+  }
+  Parse.Object.saveAll(objsToSave).then(function () {
+    res.send(objsToSave.length + " objects saved in parse");
+  }, function (err) {
+    res.status(500).send("Error in save to parse " + err.message);
+  });
+});
+
+app.post('/importParseWithDate', function (req, res) {
+  console.log("Starting to import data to parse....");
+  var json = require('./json/' + process.env.PARSE_CLASS + '.json');
+  Parse.initialize(process.env.PARSE_APPID, process.env.PARSE_JSKEY);
+  var parseObjs = json.results;
+  var objsToSave = [];
+  for (var i = 0; i < parseObjs.length; i++) {
+    var obj = parseObjs[i];
+    var createdAt = obj['createdAt'];
+    if (createdAt.indexOf(process.env.createdAt) != -1) {
       var ParseObj = Parse.Object.extend(process.env.PARSE_CLASS);
       var parseObj = new ParseObj();
       for (var key in obj) {
